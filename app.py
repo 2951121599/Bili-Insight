28590b7d566ae2ccd772c3fcbc353aac10d68e90
Qiang <wangqmshf@hotmail.com>
2023-06-10 10:53:14 +0800

    update docs

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from gpt_analyze import analyze_by_3p5
 iface = gr.Interface(fn=analyze_by_3p5, inputs=[
     gr.components.Textbox(label='Your text',
                           lines=5,
-                          placeholder='Long text need to be short',
+                          placeholder='The text that needs to be summarized',
                           max_lines=100),
     gr.components.Textbox(label='api key',
                           placeholder='apikey from openai',
